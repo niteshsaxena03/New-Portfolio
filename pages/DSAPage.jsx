1a26988550ba9d4d3290eefc22419405c2f2363c
Nitesh Saxena <nitesh.saxena03@gmail.com>
2025-02-10 22:01:39 +0530

    temp

--- pages/DSAPage.jsx
+++ pages/DSAPage.jsx
@@ -6,21 +6,21 @@ const achievements = [
     title: "ACM ICPC Regionals",
     description:
       "Qualified for ACM ICPC Kanpur Regionals 2024 after securing Rank 1 in my college.",
-    image: "/images/icpc.png",
+    image: "/images/icpc.png", // Correct path for Vercel deployment
     link: "https://icpc.global/ICPCID/HU1MX4AWA8HJ",
   },
   {
     title: "Codeforces Specialist",
     description:
       "Solved 200+ problems on Codeforces with a Specialist rating of 1421.",
-    image: "/images/codeforces.png",
+    image: "/images/codeforces.png", // Correct path for Vercel deployment
     link: "https://codeforces.com/profile/niteshsaxena003",
   },
   {
     title: "LeetCode Knight",
     description:
       "Solved 500+ problems on LeetCode and achieved a Knight rating badge with 1888 rating.",
-    image: "/images/leetcode.png",
+    image: "/images/leetcode.png", // Correct path for Vercel deployment
     link: "https://leetcode.com/u/niteshsaxena03/",
   },
 ];
